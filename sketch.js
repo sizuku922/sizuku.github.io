8cf54b4f9bffed1f43f086c9486caab48571770f
sizuku922 <aok21304@gmail.com>
2025-11-26 14:33:01 +0900

    Remove setup and draw functions from sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,1 @@
-function setup() {
-  let canvas = createCanvas(windowWidth * 0.9, 400);
-  canvas.parent('sketch-container');
-  background(100);
-}
 
-function draw() {
-  fill(255);
-  noStroke();
-  circle(mouseX, mouseY, 20);
-}
